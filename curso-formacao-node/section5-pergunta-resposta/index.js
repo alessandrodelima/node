2ed3ddf0717a4930adb768dc065db19085662969
Alessandro Lima <alessandro.lima@msn.com>
2020-11-02 17:03:45 -0300

    Criançao do perguntar.ejs, alteração de rotas e criação de partials

--- curso-formacao-node/section5-pergunta-resposta/index.js
+++ curso-formacao-node/section5-pergunta-resposta/index.js
@@ -1,11 +1,31 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+const connection = require("./database/database");
+const Pergunta = require("./database/Pergunta");
+
+
+connection
+  .authenticate()
+  .then(() => {
+    console.log("Conexão feita com o banco de dados!");
+  })
+  .catch((msgErro) => {
+    console.log("msgErro");
+  });
 const port = 4000;
 
 app.set('view engine', 'ejs'); // Estou dizendo para o Express usar o EJS como View engine.
 app.use(express.static('public'));
+//body-parser 
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
+//routers
 app.get("/", (req, res) => {
+  Pergunta.findAll({ raw: true }).then(perguntas => {
+    console.log(perguntas);
+  });
   res.render("index");
 });
 
@@ -13,9 +33,20 @@ app.get("/perguntar", (req, res) => {
   res.render("perguntar");
 });
 
-app.listen(port, () => {
-  console.log("App running!");
+app.post("/saveQuestions", (req, res) => {
+  let titulo = req.body.titulo;
+  let descricao = req.body.descricao;
+  //res.send("Formulário recebido! Titulo: "+ titulo + " Descrição: "+descricao);
+
+  Pergunta.create({
+    titulo: titulo,
+    descricao: descricao
+  }).then(() => {
+    res.redirect("/");
+  });
 });
 
-
+app.listen(port, () => {
+  console.log("App running! Porta:", port);
+});
 
